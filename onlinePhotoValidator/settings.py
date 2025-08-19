ef2b528fc26b372ad8c30c51e02fd0221c1d160e
Animesh Parajuli <animesh.parajuli.1@gmail.com>
2025-08-19 06:29:56 +0545

    ...

--- onlinePhotoValidator/settings.py
+++ onlinePhotoValidator/settings.py
@@ -134,12 +134,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# Only include this if you want a global static folder
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-
-DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
-STATIC_ROOT = os.path.join(BASE_DIR)
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
